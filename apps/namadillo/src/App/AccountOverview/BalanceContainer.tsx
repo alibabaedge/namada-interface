6f0c58cee8b869b377cd2a47370007437f037d71
Harrison Mendonça <harrisonmendonca@gmail.com>
2024-10-29 18:05:40 -0300

    feat: create BalanceContainer

--- apps/namadillo/src/App/AccountOverview/BalanceContainer.tsx
+++ apps/namadillo/src/App/AccountOverview/BalanceContainer.tsx
@@ -1,6 +1,7 @@
 import { SkeletonLoading, Stack } from "@namada/components";
 import { AtomErrorBoundary } from "App/Common/AtomErrorBoundary";
 import { BalanceChart } from "App/Common/BalanceChart";
+import { FiatCurrency } from "App/Common/FiatCurrency";
 import { NamCurrency } from "App/Common/NamCurrency";
 import BigNumber from "bignumber.js";
 import { useBalances } from "hooks/useBalances";
@@ -9,38 +10,35 @@ import { colors } from "theme";
 type NamBalanceListItemProps = {
   title: string;
   color: string;
-  amount: BigNumber;
+  nam?: BigNumber;
+  dollar?: BigNumber | null;
   isLoading: boolean;
 };
 
 const NamBalanceListItem = ({
   title,
   color,
-  amount,
+  nam,
+  dollar,
   isLoading,
 }: NamBalanceListItemProps): JSX.Element => {
   return (
-    <li
-      className="leading-5 bg-neutral-900 px-4 py-3 rounded-sm min-w-[165px]"
-      aria-description={`${title} amount is ${amount.toString()} NAM`}
-    >
+    <li className="leading-5 bg-neutral-900 px-4 py-3 rounded-sm min-w-[165px]">
       <span className="flex items-center text-xs gap-1.5">
         <i className="w-2 h-2 rounded-full" style={{ background: color }} />
         {title}
       </span>
-      {isLoading ?
-        <SkeletonLoading height="22px" width="100px" />
-      : <NamCurrency
-          amount={amount}
-          className="text-lg pl-3.5"
-          currencySymbolClassName="hidden"
-        />
-      }
+      <div className="text-lg pl-3.5">
+        {isLoading && <SkeletonLoading height="22px" width="80px" />}
+        {dollar === null && "N/A"}
+        {dollar && <FiatCurrency amount={dollar} />}
+        {nam && <NamCurrency amount={nam} currencySymbolClassName="hidden" />}
+      </div>
     </li>
   );
 };
 
-export const NamBalanceContainer = (): JSX.Element => {
+export const BalanceContainer = (): JSX.Element => {
   const {
     balanceQuery,
     stakeQuery,
@@ -72,21 +70,27 @@ export const NamBalanceContainer = (): JSX.Element => {
           />
           <Stack gap={2} as="ul">
             <NamBalanceListItem
-              title="Available NAM"
+              title="Shielded Assets"
+              color={colors.shielded}
+              dollar={shieldedAmount}
+              isLoading={isLoading}
+            />
+            <NamBalanceListItem
+              title="Transparent Assets"
               color={colors.balance}
-              amount={availableAmount}
+              nam={availableAmount} // TODO
               isLoading={isLoading}
             />
             <NamBalanceListItem
               title="Staked NAM"
               color={colors.bond}
-              amount={bondedAmount}
+              nam={bondedAmount}
               isLoading={isLoading}
             />
             <NamBalanceListItem
               title="Unbonded NAM"
               color={colors.unbond}
-              amount={unbondedAmount.plus(withdrawableAmount)}
+              nam={unbondedAmount.plus(withdrawableAmount)}
               isLoading={isLoading}
             />
           </Stack>
